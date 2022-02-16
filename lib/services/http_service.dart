492d2455ee167b07a38367415536899a15fed9ea
olawale-o <omoogunolawale@gmail.com>
2022-02-16 12:49:12 +0100

    Create HttpService

---
+++ lib/services/http_service.dart
@@ -0,0 +1,19 @@
+import 'package:http/http.dart' as http;
+
+class HttpService {
+  static final http.Client client = http.Client();
+  static const baseURI = 'http://10.0.2.2:3000/v1/api/';
+
+  Future<http.Response> get(String endPoint) async {
+    var url = Uri.parse("$baseURI$endPoint");
+    http.Response response = await client.get(url);
+    return response;
+  }
+
+  Future<http.Response> post(String endPoint) async {
+    var url = Uri.parse("$baseURI$endPoint");
+    http.Response response = await client.get(url);
+    return response;
+  }
+
+}
