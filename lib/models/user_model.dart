5f08baba8044901bc39c22f3d23669d29b290b8f
olawale-o <omoogunolawale@gmail.com>
2022-02-16 13:25:47 +0100

    Add factory constructor to UserModel

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -6,21 +6,40 @@ class UserModel {
   final String? _email;
   final String? _name;
   final String? _photoUrl;
+  final String? _password;
 
   const UserModel({
     required String uid,
     String? email,
     String? name,
     String? photoUrl,
+    String? password,
   })  : _uid = uid,
         _email = email,
         _name = name,
-        _photoUrl = photoUrl;
+        _photoUrl = photoUrl,
+        _password = password;
 
   String get uid => _uid;
   String? get email => _email;
   String? get name => _name;
   String? get photoUrl => _photoUrl;
+
+  Map<String,dynamic> toJson() {
+    final  Map<String,dynamic> data = {};
+    if (name != null) data["name"] = name;
+    if (email != null) data["email"] = email;
+    if (_password != null) data["password"] = _password;
+    return data;
+  }
+
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(
+        uid: json["id"],
+        email: json["email"],
+        name: json["username"],
+    );
+  }
 
   UserModel copyWith(
       {String? uid, String? email, String? name, String? photoUrl}) {
