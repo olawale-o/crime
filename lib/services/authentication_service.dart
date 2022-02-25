728127eec25cdf39a8b15d5a3ea35448b4236609
olawale-o <omoogunolawale@gmail.com>
2022-02-25 13:43:24 +0100

    Add thirdPartyAuthenticate to authentication service

--- lib/services/authentication_service.dart
+++ lib/services/authentication_service.dart
@@ -1,11 +1,18 @@
+import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import '../models/user_model.dart';
 import 'http_service.dart';
+import '../services/firebase_authentication_service.dart';
 
 class AuthenticationService {
   AuthenticationService(this._httpService);
   final HttpService _httpService;
+
+  StreamController<UserModel> userStream = StreamController<UserModel>();
+
+  Stream<UserModel?> get userStreamData => userStream.stream;
+
 
   Future<UserModel> authenticate(UserModel userModel) async {
     http.Response response = await _httpService.post(userModel.toJson(), "/login");
@@ -13,4 +20,12 @@ class AuthenticationService {
     var jsonResponse = result["user"];
     return UserModel.fromJson(jsonResponse);
   }
+
+  Future<UserModel> thirdPartyAuthenticate(UserModel userModel) async {
+    http.Response response = await _httpService.post(userModel.toJson(), "/third-party");
+    var result  = json.decode(response.body);
+    var jsonResponse = result["user"];
+    return UserModel.fromJson(jsonResponse);
+  }
+
 }
