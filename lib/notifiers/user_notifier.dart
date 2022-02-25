57bb0b113a9b97f64e8e9a83ba64f9410d7a63be
olawale-o <omoogunolawale@gmail.com>
2022-02-25 13:40:11 +0100

    Create streamcontroller for usermodel

--- lib/notifiers/user_notifier.dart
+++ lib/notifiers/user_notifier.dart
@@ -1,10 +1,17 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../models/user_model.dart';
+import 'dart:async';
 
 class UserNotifier extends StateNotifier<UserModel> {
-  UserNotifier(this.ref) : super(const UserModel(uid: ''));
+  UserNotifier(this.ref) : super(const UserModel(uid: '')) {
+    myController.sink.add(const UserModel(uid: ''));
+  }
 
   final Ref ref;
+
+  StreamController<UserModel?> myController = StreamController<UserModel>();
+
+  Stream<UserModel?> get myStream => myController.stream;
 
   Future<void> updateUser(UserModel user) async {
     state = state.copyWith(
@@ -13,5 +20,8 @@ class UserNotifier extends StateNotifier<UserModel> {
       name: user.name,
       photoUrl: user.photoUrl,
     );
+
+    myController.sink.add(state);
   }
+
 }
