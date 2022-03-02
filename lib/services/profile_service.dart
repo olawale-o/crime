89cfb8284ab4c3f485312054a0ca4b18236c704e
olawale-o <omoogunolawale@gmail.com>
2022-03-02 18:49:59 +0100

    Create profile service

---
+++ lib/services/profile_service.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../models/user_model.dart';
+import 'http_service.dart';
+
+class ProfileService {
+
+  ProfileService(this._httpService);
+  final HttpService _httpService;
+
+  Future<UserModel> getMyProfile(Map<String, dynamic> map) async {
+    http.Response response = await _httpService.get('user/${map['email']}', token: map['token']);
+    var data = transformRequest(response);
+    return UserModel.fromJson(data);
+  }
+
+  Future<UserModel> createProfile(Map<String, dynamic> map) async {
+    http.Response response = await _httpService.post(map['json'], "user", token: map['token']);
+    var data = transformRequest(response);
+    return UserModel.fromJson(data);
+  }
+
+  dynamic transformRequest(http.Response response) {
+    var result = json.decode(response.body);
+    return result["data"]["user"];
+  }
+}
