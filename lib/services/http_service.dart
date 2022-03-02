d324c7e4311a019b401003f1c39bd477d3a5e201
olawale-o <omoogunolawale@gmail.com>
2022-03-02 19:22:24 +0100

    Modify HttpService

--- lib/services/http_service.dart
+++ lib/services/http_service.dart
@@ -1,18 +1,25 @@
+import 'dart:io';
 import 'package:http/http.dart' as http;
 
 class HttpService {
   static final http.Client client = http.Client();
   static const baseURI = 'http://10.0.2.2:3000/v1/api/';
 
-  Future<http.Response> get(String endPoint) async {
+  Future<http.Response> get(String endPoint, {String token = ''}) async {
     var url = Uri.parse("$baseURI$endPoint");
-    http.Response response = await client.get(url);
+    var headers = {
+      HttpHeaders.authorizationHeader: 'Bearer $token'
+    };
+    http.Response response = await client.get(url, headers: headers);
     return response;
   }
 
-  Future<http.Response> post(map, String endPoint) async {
+  Future<http.Response> post(map, String endPoint, {String token = ''}) async {
     var url = Uri.parse("$baseURI$endPoint");
-    http.Response response = await client.post(url, body: map);
+    var headers = {
+      HttpHeaders.authorizationHeader: 'Bearer $token'
+    };
+    http.Response response = await client.post(url, body: map, headers: headers);
     return response;
   }
 
