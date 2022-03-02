00ba677af55731db5e668208fb217053a3f8d8f7
olawale-o <omoogunolawale@gmail.com>
2022-03-02 19:20:44 +0100

    Add getMyProfile to UserNotifier

--- lib/notifiers/user_notifier.dart
+++ lib/notifiers/user_notifier.dart
@@ -1,17 +1,19 @@
+import 'package:crime/providers/profile_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../models/user_model.dart';
 import 'dart:async';
 
+import '../providers/auth_provider.dart';
+
 class UserNotifier extends StateNotifier<UserModel> {
-  UserNotifier(this.ref) : super(const UserModel(uid: '')) {
-    myController.sink.add(const UserModel(uid: ''));
-  }
+  UserNotifier(this.ref) : super(const UserModel(uid: ''));
+  // myController.sink.add(const UserModel(uid: ''));
 
   final Ref ref;
 
-  StreamController<UserModel?> myController = StreamController<UserModel>();
+  // StreamController<UserModel?> myController = StreamController<UserModel>();
 
-  Stream<UserModel?> get myStream => myController.stream;
+  // Stream<UserModel?> get myStream => myController.stream;
 
   Future<void> updateUser(UserModel user) async {
     state = state.copyWith(
@@ -20,8 +22,13 @@ class UserNotifier extends StateNotifier<UserModel> {
       name: user.name,
       photoUrl: user.photoUrl,
     );
-
-    myController.sink.add(state);
+    // myController.sink.add(state);
   }
 
+  Future<void> getMyProfile(String email) async {
+    final currentUserToken = await ref.watch(firebaseAuthServiceProvider).currentUserToken;
+    final profileService = ref.read(profileServiceProvider);
+    var map = {'email': email, 'token': currentUserToken};
+    updateUser(await profileService.getMyProfile(map));
+  }
 }
