957d33227e73426b933824f7d1e620e0ddacf266
olawale-o <omoogunolawale@gmail.com>
2022-03-04 17:21:14 +0100

    Modify profile page

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,28 +1,27 @@
-import 'package:crime/models/user_model.dart';
-
-import '../screens/profile_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../models/user_model.dart';
+import '../screens/profile_screen.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends ConsumerWidget {
   final UserModel userModel;
   const ProfilePage({Key? key, required this.userModel }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref ) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(userModel.name.toString(), style: const TextStyle(color: Colors.black54),),
+        title: Text(userModel.name ?? "Username", style: const TextStyle(color: Colors.black54),),
         centerTitle: true,
         elevation: 0.0,
         backgroundColor: Colors.white,
         actions: [
           Padding(
             padding: const EdgeInsets.all(8.0),
-            child: CircleAvatar(
+            child: userModel.photoUrl != null ? CircleAvatar(
               radius: 15,
               backgroundImage: NetworkImage(userModel.photoUrl.toString()),
-            ),
+            ) : const CircleAvatar(radius: 15, backgroundImage: AssetImage("assets/avatar.jpg"),),
           )
         ],
       ),
