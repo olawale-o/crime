3c9f816e2f17c83aaca5a5d2f326ba05619f6e77
olawale-o <omoogunolawale@gmail.com>
2022-03-25 13:18:26 +0100

    Modify friendsSuggestionProvider

--- lib/providers/connection_provider.dart
+++ lib/providers/connection_provider.dart
@@ -1,9 +1,13 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../providers/user_provider.dart';
+import '../providers/auth_provider.dart';
 import '../models/person_model.dart';
 import '../data/datasource/remote_datasource.dart';
 
 final friendsSuggestionProvider = FutureProvider<List<PersonModel>>((ref) async {
   var dataSource = RemoteDataSource();
-  var users = await dataSource.getSuggestedFriends('user/all');
+  final currentUserToken = await ref.read(firebaseAuthServiceProvider).currentUserToken;
+  final userId = ref.read(userProvider).uid;
+  var users = await dataSource.getSuggestedFriends('user/$userId/suggestion', currentUserToken);
   return users;
 });
