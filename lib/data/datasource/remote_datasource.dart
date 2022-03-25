e814dc0c56e44534a4bec2820e51dace3725dafc
olawale-o <omoogunolawale@gmail.com>
2022-03-25 11:19:53 +0100

    Pass token along friend suggestion request

--- lib/data/datasource/remote_datasource.dart
+++ lib/data/datasource/remote_datasource.dart
@@ -3,15 +3,15 @@ import '../../services/http_service.dart';
 import '../../models/person_model.dart';
 
 abstract class DataSource {
-  Future<List<PersonModel>> getSuggestedFriends(String endPoint);
+  Future<List<PersonModel>> getSuggestedFriends(String endPoint, String token);
 }
 
 class RemoteDataSource implements DataSource {
   final HttpService httpService = HttpService();
 
   @override
-  Future<List<PersonModel>> getSuggestedFriends(String endPoint) async {
-    var response = await httpService.get(endPoint);
+  Future<List<PersonModel>> getSuggestedFriends(String endPoint, String token) async {
+    var response = await httpService.get(endPoint, token: token);
     var results = json.decode(response.body);
     List data = results['users'];
     List<PersonModel> users = data.map((user) => PersonModel.fromJson(user)).toList();
