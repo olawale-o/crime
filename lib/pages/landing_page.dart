0a2e5879afac0c44fca4e27ab24f0df564d78282
olawale-o <omoogunolawale@gmail.com>
2022-07-02 16:29:55 +0100

    Use futureUserProvider

--- lib/pages/landing_page.dart
+++ lib/pages/landing_page.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../providers/loading_provider.dart';
 import '../pages/profile_page.dart';
-import '../pages/authentication_page.dart';
-import '../providers/user_provider.dart';
 import '../models/user_model.dart';
+import '../providers/user_provider.dart';
 
 class LandingPage extends ConsumerStatefulWidget {
   final UserModel userModel;
@@ -18,21 +16,23 @@ class _LandingPageState extends ConsumerState<LandingPage> {
   @override
   void initState() {
     super.initState();
-    ref.read(userProvider.notifier).getMyProfile(widget.userModel.email!);
   }
 
   @override
   Widget build(BuildContext context) {
-    final isLoading = ref.watch(loadingProvider);
-    final user = ref.watch(userProvider);
-    return Scaffold(
-        body: isLoading.loading ? const Center(child: CircularProgressIndicator(),)
-        :  user.name == null ? const AuthenticationPage() : ProfilePage(
-          userModel: UserModel(
-            uid: user.uid,
-            name: user.name,
-            email: user.email,
-            photoUrl: user.photoUrl,
-        ),));
+    final person = ref.watch(futureUserProvider(widget.userModel.email));
+    return person.when(
+        data: (user) {
+          return ProfilePage(
+            userModel: UserModel(
+              uid: user.uid,
+              name: user.name,
+              email: user.email,
+              photoUrl: user.photoUrl,
+            ),);
+        },
+        error: (e, trace) => const Center(child: Text('error'),),
+        loading: () => const Center(child: CircularProgressIndicator(),)
+    );
   }
 }
